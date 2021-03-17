f88fe149da4fa18e4081075f235656ad734f0e3e
gyh <1013172884@qq.com>
2021-03-17 16:50:50 +0800

    在Demo中添加addMethod方法

---
+++ app/src/main/java/com/example/myapplication/gitflow/Demo.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.gitflow;
+
+public class Demo {
+    /**
+     * feature-mac
+     * addMethod
+     */
+    public void addMethod(){
+        System.out.println("2021/3/17 16:50 添加addMethod方法");
+    }
+}
